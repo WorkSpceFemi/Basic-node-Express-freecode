7bad43e02516f3d3e340be1d250889e0c258afd7
Femi Francis <CS5997@cloudium.local>
2021-09-15 15:03:20 +0530

    req client details

--- myApp.js
+++ myApp.js
@@ -25,6 +25,20 @@ app.get(
     });
   }
 );
+app.get("/:word/echo", (req, res) => {
+  const { word } = req.params;
+  res.json({
+    echo: word
+  });
+});
+app.get("/name", (req, res) => {
+  var first=req.query.first;
+  var last=req.query.last;
+  var {first:firstName,last:lastName}=req.query;
+  res.json({
+    name: `${firstName} ${lastName}`
+  });
+});
 
 app.get("/json",function(req,res){
   if(process.env.MESSAGE_STYLE==="uppercase"){
