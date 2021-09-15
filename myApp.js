0cddd6ec8f0dab3e76f8d82c347178b9872280c5
Femi Francis <CS5997@cloudium.local>
2021-09-15 10:00:35 +0530

    add css

--- myApp.js
+++ myApp.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = express();
 console.log("Hello World");
 app.get("/", function(req, res) {
-    res.send("Hello Express");
+    res.sendFile(__dirname+"/views/index.html");
   });
-
+app.use(express.static(__dirname + "/public/style.css"));
 
 
 
