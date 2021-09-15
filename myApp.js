7c96bf248e7d428e93018566fe1c37e929d54825
Femi Francis <CS5997@cloudium.local>
2021-09-15 14:04:03 +0530

    req varriables with middileware

--- myApp.js
+++ myApp.js
@@ -2,10 +2,17 @@ var express = require('express');
 var app = express();
 
 console.log("Hello World");
+
 app.get("/", function(req, res) {
     res.sendFile(__dirname+"/views/index.html");
   });
+
 app.use("/public",express.static(__dirname+"/public/"));
+app.use(function(req,res,next){
+  console.log(req.method+ " " +req.path+ " - " +req.ip);
+  next();
+});
+
 app.get("/json",function(req,res){
   if(process.env.MESSAGE_STYLE==="uppercase"){
     res.json({"message":"HELLO JSON"});
@@ -13,6 +20,7 @@ app.get("/json",function(req,res){
     res.json({"message":"Hello json"});
   }
 });
+
 
 
 
